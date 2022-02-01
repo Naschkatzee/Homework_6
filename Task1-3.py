95437b62ad7988d624361b75dfddf78da1a72d8f
Naschkatzee <tatsiana.shelepen@gmail.com>
2022-02-01 21:27:42 +0300

    Pathlib

---
+++ Task1-3.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from random import randint
+import pathlib
+
+#Path (r'C:/Users/Tatsiana/Homework_6/main.py').mkdir (exist_ok = True)
+#for i in range (10):
+#    Path ( f'main.py/'
+#        f'{randint (2020,2022)}-'
+#       f'{randint (1,12)}-'
+#       f'{randint (1,31)}.txt').touch ()
+
+contents = pathlib.Path('main.py').iterdir()
+for path in contents:
+  if path.is_file ():
+     pathlib.Path.rename = path.stem.replace('-', '/') + path.suffix
+     print (pathlib.Path.rename)
+
+
+
+
+
+
+
