fa9844196d2a8e2dc091b21af1413931849b469b
Naschkatzee <tatsiana.shelepen@gmail.com>
2022-02-19 14:45:11 +0300

    Pathlib

--- Task1-3.py
+++ Task1-3.py
@@ -2,6 +2,10 @@ import argparse
 from pathlib import Path
 from random import randint
 import pathlib
+from loguru import logger
+import logging
+
+logger.add ('debug.log', format = '{time} {level} {message}', level = 'DEBUG')
 
 #Path (r'C:/Users/Tatsiana/Homework_6/main.py').mkdir (exist_ok = True)
 #for i in range (10):
@@ -17,16 +21,23 @@ args = parser.parse_args()
 
 division = args.division
 
+@logger.catch
+def main ():
+    contents = pathlib.Path('main.py').iterdir()
+    for path in contents:
+        if path.is_file ():
+            pathlib.Path.rename = path.stem.replace('-', division) + path.suffix
+            print (pathlib.Path.rename)
 
-contents = pathlib.Path('main.py').iterdir()
-for path in contents:
-  if path.is_file ():
-     pathlib.Path.rename = path.stem.replace('-', division) + path.suffix
-     print (pathlib.Path.rename)
+main ()
 
 
+import logging
 
-
+logger = logging.getLogger()
+handler = logging.FileHandler('logs.log')
+logger.addHandler(handler)
+logger.error('Our Third Log Message')
 
 
 
