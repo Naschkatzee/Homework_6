3a64c44f0a539b096a990c8db5337689998fc219
Naschkatzee <tatsiana.shelepen@gmail.com>
2022-03-04 22:39:58 +0300

    Pathlib

--- Task1-3.py
+++ Task1-3.py
@@ -4,12 +4,13 @@ from random import randint
 import pathlib
 from loguru import logger
 import logging
+import os
 
 logger.add ('debug.log', format = '{time} {level} {message}', level = 'DEBUG')
 
-#Path (r'C:/Users/Tatsiana/Homework_6/main.py').mkdir (exist_ok = True)
+#Path ('main').mkdir (exist_ok = True)
 #for i in range (10):
-#    Path ( f'main.py/'
+#    Path ( f'main/'
 #        f'{randint (2020,2022)}-'
 #       f'{randint (1,12)}-'
 #       f'{randint (1,31)}.txt').touch ()
@@ -22,14 +23,21 @@ args = parser.parse_args()
 division = args.division
 
 @logger.catch
-def main ():
-    contents = pathlib.Path('main.py').iterdir()
-    for path in contents:
-        if path.is_file ():
-            pathlib.Path.rename = path.stem.replace('-', division) + path.suffix
-            print (pathlib.Path.rename)
+def name ():
+    path = '.\main'
+    list_of_files = list(sorted(os.listdir(path)))
+    print(list_of_files)
+    for file in list_of_files:
+        fold1 = (file.split('-')[0])
+        fold2 = (file.split('-')[1])
+        fl = (file.split('-')[2])
+        Path(f'main/{fold1}/{fold2}/').mkdir(parents=True, exist_ok=True)
+        path1 = f'.\main/{file}'
+        path2 = f'.\main/{fold1}/{fold2}/{fl}'
 
-main ()
+        os.replace(path1, path2)
+
+name ()
 
 
 import logging
